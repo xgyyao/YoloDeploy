8ec323eb2a26ac34d6761a7213d9f8e5f8c76a29
xgyyao <34907486+xgyyao@users.noreply.github.com>
2021-07-20 11:38:30 +0800

    add inference images dir

--- yolov5/pytorch/YoloV5Detector/V5Detector.py
+++ yolov5/pytorch/YoloV5Detector/V5Detector.py
@@ -4,9 +4,11 @@ from models.experimental import attempt_load
 from utils.general import non_max_suppression, scale_coords
 from utils.datasets import letterbox
 from utils.torch_utils import select_device
+from utils.plots import plot_one_box
+import random
 
 class Detector:
-    def __init__(self, weights_path, gpu_id='0'):
+    def __init__(self, weights_path, gpu_id='0', colors=None):
         self.weights = weights_path
         self.device = gpu_id
         self.device = select_device(self.device)
@@ -16,6 +18,11 @@ class Detector:
         self.m = model
         self.names = model.module.names if hasattr(
             model, 'module') else model.names
+        self.colors_random = [[random.randint(0, 255) for _ in range(3)] for _ in self.names]
+        self.colors = colors
+        if self.colors:
+            for color in self.colors:
+                self.colors_random[color] = self.colors[color]
 
     def preprocess(self, img, img_size):
         img0 = img.copy()
@@ -43,13 +50,37 @@ class Detector:
                     img.shape[2:], det[:, :4], im0.shape).round()
 
                 for *x, conf, cls_id in det:
-                    lbl = self.names[int(cls_id)]
-                    if not lbl in cls:
+                    label_name = self.names[int(cls_id)]
+                    if not label_name in cls:
                         continue
-                    x1, y1 = int(x[0]), int(x[1])
-                    x2, y2 = int(x[2]), int(x[3])
+                    x1, y1 = int(x[0].cpu().detach().numpy().tolist()), int(x[1].cpu().detach().numpy().tolist())
+                    x2, y2 = int(x[2].cpu().detach().numpy().tolist()), int(x[3].cpu().detach().numpy().tolist())
+                    cls_id = int(cls_id.cpu().detach().numpy().tolist())
+                    conf = round((conf.cpu().detach().numpy().tolist()), 4)
                     pred_boxes.append(
-                        (x1, y1, x2, y2, lbl, conf))
+                        (x1, y1, x2, y2, cls_id, label_name, conf))
         return im, pred_boxes
+
+    def draw_box(self, im, pred_boxes):
+        if not pred_boxes:
+            print("can not draw box cause no objects!!")
+        else:
+            for pred_box in pred_boxes:
+                plot_one_box(pred_box, im, label=pred_box[5]+" "+str(pred_box[6]), color=self.colors_random[int(pred_box[4])], line_thickness=3)
+        return im
+
+    def print_result(self, pred_boxes):
+        if not pred_boxes:
+            print("can not print result cause no objects!!")
+        else:
+            for i in range(len(pred_boxes)):
+                print("{}st object is {}, class id is {}, x:{},y:{},w:{},h:{},conf:{}".format(i, pred_boxes[i][5], pred_boxes[i][4],
+                                                                                              pred_boxes[i][0], pred_boxes[i][1],
+                                                                                              (pred_boxes[i][2]-pred_boxes[i][0]),
+                                                                                              (pred_boxes[i][3]-pred_boxes[i][1]),
+                                                                                              pred_boxes[i][6]))
+
+
+
 
 
--- yolov5/pytorch/demo.py
+++ yolov5/pytorch/demo.py
@@ -4,6 +4,7 @@ import os
 import sys
 import importlib
 import time
+import shutil
 BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 print(BASE_PATH)
 sys.path.append(BASE_PATH)
@@ -11,19 +12,58 @@ importlib.reload(sys)
 from YoloV5Detector.V5Detector import Detector
 
 
+def inference_single_image(weights_path, thresh, src_img, dst_img, cls, colors=None, gpu_id='0'):
+    det = Detector(weights_path, gpu_id=gpu_id, colors=colors)
+    for i in range(100):
+        t1 = time.time()
+        img = cv2.imread(src_img)
+        img_res, det_res = det.detect(img, cls, thresh)
+        t2 = (time.time() - t1) * 1000
+        print("inference time:{} ms".format(t2))
+        img_res = det.draw_box(img, det_res)
+        det.print_result(det_res)
+    cv2.imwrite(dst_img, img_res)
+
+def get_images_from_dir(imgPath):
+    imagelist = os.listdir(imgPath)
+    image_dic = []
+    for imgname in imagelist:
+        if (imgname.endswith(".jpg")):
+            imgp = imgPath + imgname
+            image_dic.append(imgp)
+    return image_dic
+
+def inference_images(weights_path, thresh, src_dir, dst_dir, cls, colors=None, gpu_id='0'):
+    det = Detector(weights_path, gpu_id=gpu_id, colors=colors)
+    images_dic = get_images_from_dir(src_dir)
+    if os.path.exists(dst_dir):
+        shutil.rmtree(dst_dir)
+    os.mkdir(dst_dir)
+    for img_dic in images_dic:
+        t1 = time.time()
+        img = cv2.imread(img_dic)
+        img_res, det_res = det.detect(img, cls, thresh)
+        t2 = (time.time() - t1) * 1000
+        print("{} inference time:{} ms".format(img_dic.split('/')[-1], t2))
+        img_res = det.draw_box(img, det_res)
+        det.print_result(det_res)
+        dst_img = dst_dir + img_dic.split('/')[-1]
+        cv2.imwrite(dst_img, img_res)
+
 
 if __name__ == '__main__':
     weights_path = "weights/5.0/yolov5s.pt"
-    image_path = "samples/images/zidane.jpg"
-
-    det = Detector(weights_path)
-    cls = ['person', 'car']
     thresh = 0.3
-    for i in range(100):
-        t1 = time.time()
-        img = cv2.imread(image_path)
-        img_res, det_res = det.detect(img, cls, thresh)
-        t2 = (time.time() - t1) * 1000
-        print("time:{} ms, res:{}".format(t2, det_res))
+    src_img = "samples/images/zidane.jpg"
+    dst_img = "samples/images/zidane_res.jpg"
+    cls = ['person', 'bus', 'horse', 'dog']
+    colors = {0: (0, 0, 255), 5: (0, 255, 0)}
+    gpu_id = '1'
+    src_dir = "samples/images/"
+    dst_dir = "samples/images/res/"
+    #inference_single_image(weights_path, thresh, src_img, dst_img, cls, colors, gpu_id)
+    inference_images(weights_path, thresh, src_dir, dst_dir, cls, colors, gpu_id)
+
+
 
 
