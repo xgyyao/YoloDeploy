39293cece8484a7457261bc8ad88fa11a93df923
xgyyao <34907486+xgyyao@users.noreply.github.com>
2021-07-21 15:37:16 +0800

    add videos inference

--- yolov5/pytorch/demo.py
+++ yolov5/pytorch/demo.py
@@ -5,6 +5,7 @@ import sys
 import importlib
 import time
 import shutil
+from tqdm import tqdm
 BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 print(BASE_PATH)
 sys.path.append(BASE_PATH)
@@ -50,6 +51,27 @@ def inference_images(weights_path, thresh, src_dir, dst_dir, cls, colors=None, g
         dst_img = dst_dir + img_dic.split('/')[-1]
         cv2.imwrite(dst_img, img_res)
 
+def inference_videos(weights_path, thresh, src_video, dst_video, cls, colors=None, gpu_id='0'):
+    det = Detector(weights_path, gpu_id=gpu_id, colors=colors)
+    cap = cv2.VideoCapture(src_video)
+
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+    total_frame = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    print("fps: {}\nsize: {}\ntotal_frame:{}".format(fps, size, total_frame))
+
+    fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+    out = cv2.VideoWriter(dst_video, fourcc, fps, size)
+
+    for i in tqdm(range(total_frame)):
+        success, frame = cap.read()
+        if not success:
+            print("this video is over!")
+            break
+        img_res, det_res = det.detect(frame, cls, thresh)
+        img_res = det.draw_box(img_res, det_res)
+        out.write(img_res)
+
 
 if __name__ == '__main__':
     weights_path = "weights/5.0/yolov5s.pt"
@@ -61,8 +83,11 @@ if __name__ == '__main__':
     gpu_id = '1'
     src_dir = "samples/images/"
     dst_dir = "samples/images/res/"
+    src_video = "samples/videos/person.mp4"
+    dst_video = "samples/videos/person_res.mp4"
     #inference_single_image(weights_path, thresh, src_img, dst_img, cls, colors, gpu_id)
-    inference_images(weights_path, thresh, src_dir, dst_dir, cls, colors, gpu_id)
+    # inference_images(weights_path, thresh, src_dir, dst_dir, cls, colors, gpu_id)
+    inference_videos(weights_path, thresh, src_video, dst_video, cls, colors, gpu_id)
 
 
 
